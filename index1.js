1595915a940e9c01573e9ba78cdb9a0afff027c1
theAbiola <iamhabibabiola@gmail.com>
2023-10-02 08:33:33 +0100

    modify JS file

--- index1.js
+++ index1.js
@@ -1,1 +1,15 @@
-import bodyparser from "body-parser"
+// import bodyparser from "body-parser";
+import superheroes from "superheroes";
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) =>{
+    let name = superheroes.random();
+    res.send(`<h1>Behold, I am ${name}</h1>`);
+})
+
+app.listen(port, () => {
+    console.timeLog(`listening on port ${port}.`);
+})
